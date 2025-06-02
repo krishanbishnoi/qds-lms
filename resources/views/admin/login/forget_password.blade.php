072d4776309fa9d06d3ef55e74d71b5a5a791da8
Krishan Gopal <QD2139@qds.com>
2025-06-02 10:10:02 +0530

    sidebar Changed for active

--- resources/views/admin/login/forget_password.blade.php
+++ resources/views/admin/login/forget_password.blade.php
@@ -8,32 +8,33 @@
                     <div class="col-lg-4 mx-auto">
                         <div class="auth-form-light text-left p-5">
                             <div class="brand-logo">
-                                <img  height="100%" width="100%"  src="{{ asset('lms-img/qdegrees-logo.svg') }}"
-                                alt="logo" />
+                                <img height="100%" width="100%" src="{{ asset('lms-img/qdegrees-logo.svg') }}"
+                                    alt="logo" />
                             </div>
                             <h4>Forget Password</h4>
                             {{-- <h6 class="font-weight-light">Sign in to continue.</h6> --}}
-                            {{ Form::open(['role' => 'form','url' => 'admin/send_password' ,'class'=>"forms-sample"]) }}
+                            {{ Form::open(['role' => 'form', 'url' => 'admin/send_password', 'class' => 'forms-sample']) }}
 
                             <div class="form-group">
                                 {{-- <label for="exampleInputUsername1">Email</label> --}}
-                                <input type="text" class="form-control" name="email" id="exampleInputUsername1" placeholder="Email">
+                                <input type="text" class="form-control" name="email" id="exampleInputUsername1"
+                                    placeholder="Email">
                                 <div class="error-message help-inline">
                                     <?php echo $errors->first('email'); ?>
                                 </div>
                             </div>
 
 
-                   <button type="submit" class="btn btn-gradient-primary me-2">Submit</button>
-                   <a  href="{{ URL::to('/admin')}}"  class="btn btn-light">Cancel</a>
+                            <button type="submit" class="btn btn-gradient-primary me-2">Submit</button>
+                            <a href="{{ URL::to('/admin/login') }}" class="btn btn-light">Cancel</a>
 
-                    {{ Form::close() }}
+                            {{ Form::close() }}
+                        </div>
                     </div>
                 </div>
             </div>
+            <!-- content-wrapper ends -->
         </div>
-        <!-- content-wrapper ends -->
-    </div>
 
 
-@stop
+    @stop
